97e498dc6f2a8a51286c0d8b8ee76de1952a3026
Matteo Morganti <matteo99morganti@gmail.com>
2022-02-25 15:13:11 +0100

    push

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <SearchComponents @search="searchEmit"/>
+</template>
+
+<script>
+import SearchComponents from './SearchComponents.vue';
+
+export default {
+    name: 'MyHeader',
+    components: {
+        SearchComponents,
+    },
+    methods: {
+        searchEmit(text) {
+            this.$emit("search", text);
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+  
+</style>
---
+++ src/components/SearchComponents.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="container-fluid d-flex justify-content-between align-items-center">
+        <div class="logo">
+            <h2>Boolflix</h2>
+        </div>
+        <div class="p-3 mx-5 d-flex align-items-center">
+
+            <input type="text" placeholder="Cerca Film o Serie Tv..." class="px-5 me-3" v-model.trim="search" @keyup="searchEmit">
+
+            <button class="btn btn-danger" @click="searchEmit">Cerca</button>
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SearchComponents',
+    data() {
+        return {
+            search: '',
+        };
+    },
+    methods: {
+        searchEmit() {
+            this.$emit("search", this.search);
+        },
+     },
+};
+</script>
+
+
+<style lang="scss" scoped>
+    @import '../assets/style/vars.scss';
+
+    section{
+        background-color: $bgColorHeader;
+    }
+
+
+</style>
